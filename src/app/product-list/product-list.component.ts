8cb3ccb5543554812e438904fb3c9723ce837d09
moises dias <moises.dias16@outlook.com>
2020-02-17 13:32:48 -0300

    starting to add validators in the form

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,19 +12,6 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-// ];
-
 
 @Component({
   selector: 'product-list',
@@ -40,8 +27,6 @@ export class ProductListComponent implements OnInit {
     private productService: ProductService,
     public dialog: MatDialog
   ) { }
-
-  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
   productsList: Product[];
 
@@ -88,7 +73,6 @@ export class ProductListComponent implements OnInit {
     console.log(row);
   }
 
-
   checkSold(sold: boolean): boolean {
     if (!sold && this.onlySold) {
       return false
@@ -98,6 +82,5 @@ export class ProductListComponent implements OnInit {
     }
     return true
   }
-
 
 }
