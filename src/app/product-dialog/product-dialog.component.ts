22bd807467e588e1a354decaa223ad5ee497f817
moises <moisesdepaulodias@gmail.com>
2020-02-17 23:30:22 -0300

    delete product function

--- src/app/product-dialog/product-dialog.component.ts
+++ src/app/product-dialog/product-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material';
 import { Product } from '../product.model';
+import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-product-dialog',
@@ -9,9 +10,16 @@ import { Product } from '../product.model';
 })
 export class ProductDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Product) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Product,
+    private productService: ProductService
+    ) { }
 
   ngOnInit() {
+  }
+
+  delete(id: string) {
+    this.productService.deleteProduct(id);
   }
 
 }
